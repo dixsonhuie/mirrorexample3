239829502043758f4d5b761bf3e7b65227e2e2bb
dixson <dixson.huie@gmail.com>
2020-02-04 16:03:33 -0500

    checkin4

--- feeder/src/main/resources/feeder.sh
+++ feeder/src/main/resources/feeder.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+PROJ_DIR="/home/jay/work/gigaspace/mirrorexample3"
+
 export GS_LOOKUP_LOCATORS="localhost:4174"
 export GS_LOOKUP_GROUPS="xap-15.0.0"
 
-java -jar /home/jay/work/gigaspace/mirrorexample3/feeder/target/feeder-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar $PROJ_DIR/feeder/target/feeder-1.0-SNAPSHOT-jar-with-dependencies.jar
