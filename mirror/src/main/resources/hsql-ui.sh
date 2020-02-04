bb279c8f621b1f34cbabddd61dfed91a5ea990ff
dixsonhuie <dixson.huie@gmail.com>
2020-02-04 16:09:05 -0500

    Delete hsql-ui.sh

--- mirror/src/main/resources/hsql-ui.sh
+++
@@ -1,4 +0,0 @@
-# This is an example of how to run the hsql UI
-
-java -cp /home/jay/.m2/repository/org/hsqldb/hsqldb/2.4.1/hsqldb-2.4.1.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:postgresql://localhost/my_database
-
