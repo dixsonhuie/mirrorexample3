0698327fa4091c3ac9fb7e8c707b20d03475b3ca
dixson <dixson.huie@gmail.com>
2020-02-04 14:26:44 -0500

    checkin3

--- feeder/src/main/resources/feeder.sh
+++ feeder/src/main/resources/feeder.sh
@@ -3,4 +3,4 @@
 export GS_LOOKUP_LOCATORS="localhost:4174"
 export GS_LOOKUP_GROUPS="xap-15.0.0"
 
-java -jar feeder-1.0-SNAPSHOT.jar
+java -jar /home/jay/work/gigaspace/mirrorexample3/feeder/target/feeder-1.0-SNAPSHOT-jar-with-dependencies.jar
--- mirror/src/main/resources/hsql-ui.sh
+++ mirror/src/main/resources/hsql-ui.sh
@@ -1,4 +1,4 @@
 # This is an example of how to run the hsql UI
 
-java -cp /home/dixson/.m2/repository/org/hsqldb/hsqldb/2.4.1/hsqldb-2.4.1.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/testDB
+java -cp /home/jay/.m2/repository/org/hsqldb/hsqldb/2.4.1/hsqldb-2.4.1.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:postgresql://localhost/my_database
 
